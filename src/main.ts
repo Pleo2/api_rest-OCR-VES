ce6c61dac4c6a0f4e3f15399e83b31dde3b8b853
pleo2 <leooel23m@gmail.com>
2025-08-10 17:51:43 -0400

    feat: add settings.json to the project and add fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    // Use FastifyAdapter for better performance
+    // You can also pass options to FastifyAdapter if needed
+    new FastifyAdapter({ logger: true }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error(err);
+});
